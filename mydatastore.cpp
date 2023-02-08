2f3a9ab349260069b5e7ec39f1d49ae5dc4cc255
Edward Zhang <edwardzh@usc.edu>
2023-02-08 03:02:12 +0000

    test include

---
+++ mydatastore.cpp
@@ -0,0 +1,112 @@
+#include"mydatastore.h"
+#include<set>
+#include<iostream>
+#include"util.h"
+using namespace std;
+
+/*** 
+ ** Adds a product to the data store
+*/
+
+//Constructor that setup the datasture keywordMap
+MyDataStore::MyDataStore(){
+
+}
+
+
+void MyDataStore::addProduct(Product* p)
+{
+    //everytime adding a product, update the keyword map for searching purpose
+    product_.push_back(p);
+
+    set<string>keywordInProduct = p->keywords();
+    set<string>::iterator it;
+    //when the size is 0, every keyword in products would create a new pairs
+    if(product_.size()==0){
+        //iterate each of the keyword and create pairs and insert into the keyword map
+        for(it = keywordInProduct.begin(); it != keywordInProduct.end(); ++it){
+            set<Product*> tempSet; tempSet.insert(p);//vector that contains the product
+            pair<string,set<Product*>>p1(*it,tempSet);
+            keywordMap.insert(p1);
+        }
+    }
+    else{
+        //if some products already exist, when go go through the keywordInProduct, need to check if that keyword already existed in keywordMap
+        for(it = keywordInProduct.begin(); it != keywordInProduct.end(); ++it){
+            if(keywordMap.find(*it) != keywordMap.end()){//if not find,
+                ((*(keywordMap.find(*it))).second).insert(p);
+            }
+            else{//if find it! push it to the 
+                set<Product*> tempSet; tempSet.insert(p);//create new set that contains the product and insert to the map
+                pair<string,set<Product*>> p1(*it,tempSet);
+                keywordMap.insert(p1);
+            }
+        }
+    }
+}
+
+/**
+     * Adds a user to the data store
+     */
+void MyDataStore::addUser(User* u) 
+{
+    user_.push_back(u);
+}
+
+ /**
+     * Performs a search of products whose keywords match the given "terms"
+     *  type 0 = AND search (intersection of results for each term) while
+     *  type 1 = OR search (union of results for each term)
+     */
+
+//std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
+//std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
+vector<Product*> MyDataStore::search(vector<string>& terms, int type)
+{
+    //iterate over each of the terms.
+    int size = terms.size();
+    vector<Product*> p;
+    //if only 1 term
+    if(size == 1){
+        map<string,set<Product*>>::iterator itr;
+        itr = find(keywordMap.begin(),keywordMap.end(),terms[0]);
+        if (itr != keywordMap.end()){
+            vector<Product*> vectorOfProducts((*(keywordMap.find(terms[0]))).second.begin(),(*(keywordMap.find(terms[0]))).second.end());
+            return vectorOfProducts;
+        }
+    }
+    //if 2 items, then simply find intersection between the 2 vector
+    else if(size ==2){
+        set<Product*> setOfProducts= setUnion((*(keywordMap.find(terms[0]))).second,(*(keywordMap.find(terms[1]))).second);
+        vector<Product*> vectorOfProducts(setOfProducts.begin(),setOfProducts.end());
+        return vectorOfProducts;
+    }
+    
+        //first find the union of first 2 sets
+        set<Product*> tempSet = setUnion((*(keywordMap.find(terms[0]))).second,(*(keywordMap.find(terms[1]))).second);
+        for(int i = 2; i<size; i++){
+        tempSet = setUnion(tempSet,(*(keywordMap.find(terms[i]))).second);//update the tempSet to the newest intersection between itself and next set of products
+        }
+        vector<Product*> vectorOfProducts(tempSet.begin(),tempSet.end());
+        return vectorOfProducts;
+    
+    
+    
+}
+
+void MyDataStore::dump(ostream& ofile)
+{
+    ofile<<"<products>"<<endl;
+    int size = product_.size();
+    for(int i=0 ;i<size;i++){
+        product_[i]->dump(ofile);
+    }
+    ofile<<"</products>"<<endl;
+
+
+    ofile<<"<users>"<<endl;
+    for(int i=0 ;i<size;i++){
+        user_[i]->dump(ofile);
+    }
+    ofile<<"</users>"<<endl;
+}
---
+++ mydatastore.h
@@ -0,0 +1,27 @@
+#ifndef MYDATASTORE_H
+#define MYDATASTORE_H
+#include"datastore.h"
+#include<vector>
+#include<map>
+
+class MyDataStore : public DataStore
+{
+public:
+    MyDataStore();//constructor to setup the keywordMap structure
+
+    void addProduct(Product* p);
+
+    void addUser(User* u);
+
+    std::vector<Product*> search(std::vector<std::string>& terms, int type);
+
+    void dump(std::ostream& ofile);
+
+private:
+    std::vector<Product*> product_;//the set containing products
+    std::vector<User*> user_;//the set containing users
+    std::map<std::string, std::set<Product*>> keywordMap;
+    
+};
+
+#endif
